4ea6793721da6007d0fac0130b9e0d7172322adc
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-02-12 14:56:17 -0800

    Complete project setup

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+	title: "Trip Report",
+	description: "Discover and share your favorite trips",
+};
+
+const RootLayout = ({ children }) => {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+				<main className="app">{children}</main>
+			</body>
+		</html>
+	);
+};
+
+export default RootLayout;
