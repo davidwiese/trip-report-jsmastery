ca0efc923ef9f669ae0990d23f4acaea6fcb288e
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-02-12 15:11:25 -0800

    Add initial home page barebones

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<section className="w-full flex-center flex-col">
+			<h1 className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">
+				Discover & Share
+				<br />
+				<span className="orange_gradient text-center">
+					Hikes, routes, and climbs
+				</span>
+			</h1>
+			<p className="text-center mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl">
+				Find out where others are hiking and learn about current conditions.
+			</p>
+		</section>
+	);
 };
 
 export default Home;
