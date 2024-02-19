1ef1e09b884adc4021baf15d336e4a73d0be2830
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-02-19 10:34:48 -0800

    Add cancel and create buttons

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,8 +7,50 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 				<span className="blue_gradient">{type} Report</span>
 			</h1>
 			<p className="desc text-left max-w-md">
-				{type} and share amazing trips with the world.
+				{type} and share amazing trips with the world, and let your imagination
+				run wild with any AI-powered platform.
 			</p>
+
+			<form
+				onSubmit={handleSubmit}
+				className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+			>
+				<label>
+					<span className="font-satoshi font-semibold text-base text-gray-700">
+						Your AI Prompt
+					</span>
+					<textarea
+						value={post.prompt}
+						onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+						placeholder="Write your prompt here..."
+						required
+						className="form_textarea"
+					/>
+				</label>
+				<label>
+					<span className="font-satoshi font-semibold text-base text-gray-700">
+						Tag{" "}
+						<span className="font-normal">
+							(#product, #webdevelopment, #idea)
+						</span>
+					</span>
+					<input
+						value={post.tag}
+						onChange={(e) => setPost({ ...post, tag: e.target.value })}
+						placeholder="#tag"
+						required
+						className="form_input"
+					/>
+				</label>
+				<div className="flex-end mx-3 mb-5 gap-4">
+					<Link className="text-gray-500 text-sm" href="/">
+						Cancel
+					</Link>
+					<button type="submit" disabled={submitting}>
+						{submitting ? `${type}...` : type}
+					</button>
+				</div>
+			</form>
 		</section>
 	);
 };
