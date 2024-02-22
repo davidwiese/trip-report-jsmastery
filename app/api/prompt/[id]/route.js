d1cd1f8ac123df8fdf6ba09201e1b3ee93db4423
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-02-22 10:08:37 -0800

    Add prompt PATCH route

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -15,6 +15,19 @@ export const GET = async (req, { params }) => {
 	}
 };
 
-// PATCH (update)
-
-// DELETE (delete)
+export const PATCH = async (req, { params }) => {
+	const { prompt, tag } = await request.json();
+	try {
+		await connectToDB();
+		const existingPrompt = await Prompt.findById(params.id);
+		if (!existingPrompt) {
+			return new Response("Prompt not found", { status: 404 });
+		}
+		existingPrompt.prompt = prompt;
+		existingPrompt.tag;
+		await existingPrompt.save();
+		return new Response(JSON.stringify(existingPrompt), { status: 200 });
+	} catch (error) {
+		return new Response("Failed to update prompt", { status: 500 });
+	}
+};
